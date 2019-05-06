a466d73a1706743a404d12508a8d982e6b3fba60
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-05-07 00:16:21 +0300

    Refactor fp16 train

--- fit_predict_fp16.py
+++ fit_predict_fp16.py
@@ -6,134 +6,21 @@ import os
 from datetime import datetime
 
 import cv2
-import torch
 import numpy as np
-
-from catalyst.dl.callbacks import UtilsFactory, LossCallback, SchedulerCallback, CheckpointCallback, \
-    get_optimizer_momentum, RunnerState, Dict, Callback, GRAD_CLIPPERS
-from catalyst.dl.experiments import SupervisedRunner, SupervisedExperiment
-from torch.backends import cudnn
-from torch.optim import Adam
-
-from pytorch_toolbelt.utils.catalyst_utils import ShowPolarBatchesCallback, EpochJaccardMetric, PixelAccuracyMetric
+import torch
+from apex import amp
+from catalyst.dl.callbacks import UtilsFactory
+from catalyst.dl.experiments import SupervisedRunner
 from pytorch_toolbelt.utils import fs
+from pytorch_toolbelt.utils.catalyst_utils import ShowPolarBatchesCallback, EpochJaccardMetric, PixelAccuracyMetric
 from pytorch_toolbelt.utils.random import set_manual_seed
 from pytorch_toolbelt.utils.torch_utils import maybe_cuda, count_parameters
+from torch.backends import cudnn
+from torch.optim import Adam
 from tqdm import tqdm
 
 from common.dataset import get_dataloaders
 from common.factory import get_model, get_loss, get_optimizer, visualize_inria_predictions, predict
-
-try:
-    from apex.parallel import DistributedDataParallel as DDP
-    from apex.fp16_utils import *
-    from apex import amp, optimizers
-    from apex.multi_tensor_apply import multi_tensor_applier
-except ImportError:
-    raise ImportError("Please install apex from https://www.github.com/nvidia/apex to run this example.")
-
-
-class AmpOptimizerCallback(Callback):
-    """
-    Optimizer callback, abstraction over optimizer step.
-    """
-
-    def __init__(self,
-                 grad_clip_params: Dict = None,
-                 accumulation_steps: int = 1,
-                 optimizer_key: str = None,
-                 loss_key: str = None
-                 ):
-        """
-        @TODO: docs
-        """
-
-        grad_clip_params = grad_clip_params or {}
-        self.grad_clip_fn = GRAD_CLIPPERS.get_from_params(**grad_clip_params)
-
-        self.accumulation_steps = accumulation_steps
-        self.optimizer_key = optimizer_key
-        self.loss_key = loss_key
-        self._optimizer_wd = 0
-        self._accumulation_counter = 0
-
-    def on_stage_start(self, state: RunnerState):
-        optimizer = state.get_key(
-            key="optimizer", inner_key=self.optimizer_key
-        )
-        assert optimizer is not None
-        lr = optimizer.defaults["lr"]
-        momentum = get_optimizer_momentum(optimizer)
-        state.set_key(lr, "lr", inner_key=self.optimizer_key)
-        state.set_key(momentum, "momentum", inner_key=self.optimizer_key)
-
-    def on_epoch_start(self, state):
-        optimizer = state.get_key(
-            key="optimizer", inner_key=self.optimizer_key
-        )
-        self._optimizer_wd = optimizer.param_groups[0].get("weight_decay", 0.0)
-        optimizer.param_groups[0]["weight_decay"] = 0.0
-
-    @staticmethod
-    def grad_step(*, optimizer, optimizer_wd=0, grad_clip_fn=None):
-        for group in optimizer.param_groups:
-            if optimizer_wd > 0:
-                for param in group["params"]:
-                    param.data = param.data.add(
-                        -optimizer_wd * group["lr"], param.data
-                    )
-            if grad_clip_fn is not None:
-                grad_clip_fn(group["params"])
-        optimizer.step()
-
-    def on_batch_end(self, state):
-        if not state.need_backward:
-            return
-
-        self._accumulation_counter += 1
-        model = state.model
-        optimizer = state.get_key(
-            key="optimizer", inner_key=self.optimizer_key
-        )
-        loss = state.get_key(key="loss", inner_key=self.loss_key)
-
-        with amp.scale_loss(loss, optimizer) as scaled_loss:
-            scaled_loss.backward()
-
-        if (self._accumulation_counter + 1) % self.accumulation_steps == 0:
-            self.grad_step(
-                optimizer=optimizer,
-                optimizer_wd=self._optimizer_wd,
-                grad_clip_fn=self.grad_clip_fn
-            )
-            model.zero_grad()
-            self._accumulation_counter = 0
-
-    def on_epoch_end(self, state):
-        optimizer = state.get_key(
-            key="optimizer", inner_key=self.optimizer_key
-        )
-        optimizer.param_groups[0]["weight_decay"] = self._optimizer_wd
-
-
-class AmpExperiment(SupervisedExperiment):
-
-    def get_callbacks(self, stage: str) -> "List[Callback]":
-        callbacks = self._callbacks
-        if not stage.startswith("infer"):
-            default_callbacks = [
-                (self._criterion, LossCallback),
-                (self._optimizer, AmpOptimizerCallback),
-                (self._scheduler, SchedulerCallback),
-                ("_default_saver", CheckpointCallback),
-            ]
-
-            for key, value in default_callbacks:
-                is_already_present = any(
-                    isinstance(x, value) for x in callbacks)
-                if key is not None and not is_already_present:
-                    callbacks.append(value())
-        return callbacks
 
 
 def main():
@@ -198,11 +85,10 @@ def main():
     loaders["train"] = train_loader
     loaders["valid"] = valid_loader
 
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20, 40, 60, 90, 120], gamma=0.3)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20, 40, 60, 90, 120], gamma=0.5)
 
     # model runner
     runner = SupervisedRunner()
-    runner._default_experiment = AmpExperiment
 
     if args.checkpoint:
         checkpoint = UtilsFactory.load_checkpoint(fs.auto_file(args.checkpoint))
@@ -218,7 +104,7 @@ def main():
     log_dir = os.path.join('runs', prefix)
     os.makedirs(log_dir, exist_ok=False)
 
-    print('Train session    :', prefix)
+    print('Train session      :', prefix)
     print('\tFast mode      :', args.fast)
     print('\tTrain mode     :', train_mode)
     print('\tEpochs         :', num_epochs)
@@ -228,14 +114,14 @@ def main():
     print('\tAugmentations  :', augmentations)
     print('\tTrain size     :', len(train_loader), len(train_loader.dataset))
     print('\tValid size     :', len(valid_loader), len(valid_loader.dataset))
-    print('Model            :', model_name)
+    print('Model              :', model_name)
     print('\tParameters     :', count_parameters(model))
     print('\tImage size     :', image_size)
-    print('Optimizer        :', optimizer_name)
+    print('Optimizer          :', optimizer_name)
     print('\tLearning rate  :', learning_rate)
     print('\tBatch size     :', batch_size)
     print('\tCriterion      :', args.criterion)
-    print('AMP Model        :', args.fp16_opt_level)
+    print('AMP Model          :', args.fp16_opt_level)
 
     # model training
     runner.train(
