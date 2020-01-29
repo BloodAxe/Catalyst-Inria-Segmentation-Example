6f064b589fbda45f9b0bf7e7b11d79475e22e77e
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-01-29 15:55:31 +0200

    hrnet48_unet64

--- inria/models/__init__.py
+++ inria/models/__init__.py
@@ -21,6 +21,7 @@ def get_model(model_name: str, dropout=0.0, pretrained=True) -> nn.Module:
         "seresnext50_unet64": unet.seresnext50_unet64,
         "hrnet18_unet64": unet.hrnet18_unet64,
         "hrnet34_unet64": unet.hrnet34_unet64,
+        "hrnet48_unet64": unet.hrnet48_unet64,
 
         # Deeplab
         "resnet34_deeplab128": deeplab.resnet34_deeplab128,
