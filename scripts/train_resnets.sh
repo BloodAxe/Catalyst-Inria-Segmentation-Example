8ddc01117bd2b3e744f2cb72c3034336c82f0989
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2020-08-18 13:07:17 +0300

    Catalyst

--- scripts/train_resnets.sh
+++ scripts/train_resnets.sh
@@ -4,4 +4,6 @@ python -m torch.distributed.launch --nproc_per_node=4 fit_predict.py --fp16 -w 8
   -b 10 -m resnet101_unet64 --size 512\
   -o RAdam -lr 3e-4 -wd 1e-5  -a hard\
   -e 50 -s cos2\
+  --criterion bce 1\
+  --criterion dice 1\
   --train-mode tiles --show
