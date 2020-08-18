c1f9ef9ad021de5f7c3fd2b00e0ff4e7ac0d8286
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2020-08-18 13:20:34 +0300

    Catalyst

--- scripts/train_resnets.sh
+++ scripts/train_resnets.sh
@@ -1,7 +1,7 @@
 export INRIA_DATA_DIR="/home/bloodaxe/datasets/AerialImageDataset"
 
 python -m torch.distributed.launch --nproc_per_node=4 fit_predict.py --fp16 -w 8\
-  -b 10 -m resnet101_unet64 --size 512\
+  -b 16 -m resnet101_unet64 --size 512\
   -o RAdam -lr 3e-4 -wd 1e-5  -a hard\
   -e 50 -s cos2\
   --criterion bce 1\
