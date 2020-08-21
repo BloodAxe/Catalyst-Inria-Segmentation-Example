bf49845c0ab858f0af16b6bb4c3dfe0ed7579bdc
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2020-08-21 14:04:05 +0300

    Allow to override dropout

--- inria/models/__init__.py
+++ inria/models/__init__.py
@@ -5,8 +5,8 @@ from . import fpn, unet, deeplab, hrnet, hg, can
 __all__ = ["get_model"]
 
 
-def get_model(model_name: str, dropout=0.0, pretrained=True) -> nn.Module:
+def get_model(model_name: str, pretrained=True, **kwargs) -> nn.Module:
     from catalyst.dl import registry
 
     model_fn = registry.MODEL.get(model_name)
-    return model_fn(dropout=dropout, pretrained=pretrained)
+    return model_fn(pretrained=pretrained, **kwargs)
