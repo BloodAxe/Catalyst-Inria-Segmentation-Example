5ec01b78e154da11035ea0bfdcae620f5164d2b6
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2020-08-31 11:00:11 +0300

    b6_unet32_s2_bi

--- scripts/train_b6.sh
+++ scripts/train_b6.sh
@@ -8,10 +8,10 @@ export INRIA_DATA_DIR="/home/bloodaxe/datasets/AerialImageDataset"
 python -m torch.distributed.launch --nproc_per_node=4 fit_predict.py -w 6 --fp16 -v\
   -b 6 -m b6_unet32_s2_tc\
   --train-mode tiles --size 512 -s cos -o RAdam -a hard -lr 3e-4 -e 50 --seed 555\
-  --criterion bce 1 --criterion dice 1 -c /home/bloodaxe/develop/Catalyst-Inria-Segmentation-Example/runs/200829_17_44_b6_unet32_s2_fp16_local_rank_0/main/checkpoints_optimized_jaccard/best.pth
+  --criterion bce 1 --criterion dice 1 --transfer /home/bloodaxe/develop/Catalyst-Inria-Segmentation-Example/runs/200829_17_44_b6_unet32_s2_fp16_local_rank_0/main/checkpoints_optimized_jaccard/best.pth
 
 export INRIA_DATA_DIR="/home/bloodaxe/datasets/AerialImageDataset"
 python -m torch.distributed.launch --nproc_per_node=4 fit_predict.py -w 6 --fp16 -v\
   -b 6 -m b6_unet32_s2_rdtc\
   --train-mode tiles --size 512 -s cos -o RAdam -a hard -lr 3e-4 -e 50 --seed 555\
-  --criterion bce 1 --criterion dice 1 -c /home/bloodaxe/develop/Catalyst-Inria-Segmentation-Example/runs/200829_17_44_b6_unet32_s2_fp16_local_rank_0/main/checkpoints_optimized_jaccard/best.pth
+  --criterion bce 1 --criterion dice 1 --transfer /home/bloodaxe/develop/Catalyst-Inria-Segmentation-Example/runs/200829_17_44_b6_unet32_s2_fp16_local_rank_0/main/checkpoints_optimized_jaccard/best.pth
