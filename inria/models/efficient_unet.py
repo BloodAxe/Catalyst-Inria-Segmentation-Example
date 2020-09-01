bfc3fb8a19c2640ee6ea7fe187858535a25d6209
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-09-01 07:54:46 +0300

    Migrate to catalyst 20.8.2

--- inria/models/efficient_unet.py
+++ inria/models/efficient_unet.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from functools import partial
 from typing import Union, List, Dict
 
-from pytorch_toolbelt.modules import conv1x1, UnetBlock, ACT_RELU, ABN, ACT_SWISH, Swish
+from pytorch_toolbelt.modules import conv1x1, UnetBlock, ACT_RELU, ABN, ACT_SWISH, Swish, DropBlock2D
 from pytorch_toolbelt.modules import encoders as E
 from pytorch_toolbelt.modules.decoders import UNetDecoder
 from pytorch_toolbelt.modules.encoders import EncoderModule
@@ -13,6 +13,7 @@ from timm.models.efficientnet_blocks import InvertedResidual
 from ..dataset import OUTPUT_MASK_KEY
 from catalyst.registry import Model
 
+
 __all__ = [
     "EfficientUnetBlock",
     "EfficientUnetSegmentationModel",
@@ -21,23 +22,40 @@ __all__ = [
 
 
 class EfficientUnetBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, abn_block=ABN):
+    def __init__(self, in_channels: int, out_channels: int, activation=Swish, drop_path_rate=0.0):
         super().__init__()
-        mid_channels = (in_channels + out_channels) // 2
-        self.conv1 = InvertedResidual(in_channels, mid_channels, act_layer=Swish, se_ratio=0.25, exp_ratio=4)
-        self.conv2 = InvertedResidual(mid_channels, out_channels, act_layer=Swish, se_ratio=0.25, exp_ratio=4)
+        self.ir = InvertedResidual(in_channels, out_channels, act_layer=activation, se_ratio=0.25, exp_ratio=4)
+        self.drop = DropBlock2D(drop_path_rate, 2)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            activation(inplace=True),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            activation(inplace=True),
+        )
 
     def forward(self, x):
+        x = self.ir(x)
+        x = self.drop(x)
         x = self.conv1(x)
         x = self.conv2(x)
         return x
 
 
 class EfficientUNetDecoder(UNetDecoder):
-    def __init__(self, feature_maps: List[int], decoder_features: List[int], upsample_block=nn.UpsamplingNearest2d):
+    def __init__(
+        self,
+        feature_maps: List[int],
+        decoder_features: List[int],
+        upsample_block=nn.UpsamplingNearest2d,
+        activation=Swish,
+    ):
         super().__init__(
             feature_maps,
-            unet_block=EfficientUnetBlock,
+            unet_block=partial(EfficientUnetBlock, activation=activation, drop_path_rate=0.2),
             decoder_features=decoder_features,
             upsample_block=upsample_block,
         )
@@ -51,12 +69,14 @@ class EfficientUnetSegmentationModel(nn.Module):
         num_classes: int = 1,
         dropout=0.25,
         full_size_mask=True,
-        activation=ACT_RELU,
+        activation=Swish,
     ):
         super().__init__()
         self.encoder = encoder
 
-        self.decoder = EfficientUNetDecoder(feature_maps=encoder.channels, decoder_features=unet_channels)
+        self.decoder = EfficientUNetDecoder(
+            feature_maps=encoder.channels, decoder_features=unet_channels, activation=activation
+        )
 
         self.mask = nn.Sequential(
             OrderedDict([("drop", nn.Dropout2d(dropout)), ("conv", conv1x1(self.decoder.channels[0], num_classes))])
@@ -66,11 +86,11 @@ class EfficientUnetSegmentationModel(nn.Module):
 
     def forward(self, x: Tensor) -> Dict[str, Tensor]:
         x_size = x.size()
-        x = self.encoder(x)
-        x = self.decoder(x)
+        enc = self.encoder(x)
+        dec = self.decoder(enc)
 
         # Decode mask
-        mask = self.mask(x[0])
+        mask = self.mask(dec[0])
 
         if self.full_size_mask:
             mask = F.interpolate(mask, size=x_size[2:], mode="bilinear", align_corners=False)
@@ -86,5 +106,5 @@ def b4_effunet32_s2(input_channels=3, num_classes=1, dropout=0.2, pretrained=Tru
         encoder.change_input_channels(input_channels)
 
     return EfficientUnetSegmentationModel(
-        encoder, num_classes=num_classes, unet_channels=[32, 64, 128, 256], activation=ACT_SWISH, dropout=dropout
+        encoder, num_classes=num_classes, unet_channels=[32, 64, 128, 256], activation=Swish, dropout=dropout
     )
