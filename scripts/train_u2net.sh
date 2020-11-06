b99acbe81e5d671e64b4fa55fca95da8396ca6c1
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2020-11-06 20:53:21 +0200

    U2NET

--- scripts/train_u2net.sh
+++ scripts/train_u2net.sh
@@ -7,6 +7,6 @@
 
 export INRIA_DATA_DIR="/home/bloodaxe/datasets/AerialImageDataset"
 python -m torch.distributed.launch --nproc_per_node=4 fit_predict.py -w 6 --fp16 -v\
-  -b 4 -m U2NET\
+  -b 8 -m U2NET --show\
   --train-mode tiles --size 512 -s cos -o RAdam -a hard -lr 3e-4 -e 50 --seed 555\
   --criterion bce 1 --criterion dice 1
